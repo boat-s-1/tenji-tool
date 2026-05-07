b8be9d7222fe0a22d603e83052d8b0a8cf8ad068
boat-s-1 <marron.toyohashi@gmail.com>
2026-05-08 05:35:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -883,7 +883,7 @@ if current_stamp_src:
          style="width: 220px; 
                 height: auto; 
                 position: absolute; 
-                right: -80px;    /* 右端ギリギリか、少しはみ出すくらい */
+                right: 100px;    /* 右端ギリギリか、少しはみ出すくらい */
                 top: -30px;      /* 上にはみ出すくらい */
                 transform: rotate(-15deg); 
                 z-index: 100; 
