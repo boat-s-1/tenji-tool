898b4c2500481748ae4ce35bfb9fcf8c69621cf1
boat-s-1 <marron.toyohashi@gmail.com>
2026-05-08 21:39:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -167,5 +167,5 @@ html_code2 = f"""
 
 # 表示
 tab1, tab2 = st.tabs(["📰 前日版", "🌸 直前版"])
-with tab1: html(html_code, height=2200, scrolling=True)
+with tab1: html(html_code, height=2500, scrolling=True)
 with tab2: html(html_code2, height=1800, scrolling=True)
